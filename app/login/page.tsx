0b4aaaddc94223fbc55c53287f8ce85761e8940f
William-Cossa <devwilliamcossa@gmail.com>
2024-06-27 23:46:00 +0200

    feat: add react-hook-form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { Button } from "@/components/ui/button"
+'use client'
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -7,40 +8,46 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 function Login() {
-     
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  function handleLogin(){
+    console.log("Login:", email, password);
+  }
+
+
   return (
     <div>
-        <Card className="w-[350px]">
-      <CardHeader className='flex justify-center items-center'>
-        <CardTitle>Login</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Email</Label>
-              <Input id="email" placeholder="E-mail" />
+      <Card className="w-[350px]">
+        <CardHeader className="flex justify-center items-center">
+          <CardTitle>Login</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Email</Label>
+                <Input id="email" placeholder="E-mail" onChange={(e)=>setEmail(e.target.value)} />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="pasword">Password</Label>
+                <Input id="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)} />
+              </div>
             </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="pasword">Password</Label>
-              <Input id="password" placeholder="Password" />
-            </div>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter className="">
-        {/* <Button variant="outline">Cancel</Button> */}
-        <Button className='w-full'>Entrar</Button>
-      </CardFooter>
-    </Card>
+          </form>
+        </CardContent>
+        <CardFooter className="">
+          {/* <Button variant="outline">Cancel</Button> */}
+          <Button className="w-full" onClick={handleLogin}>Entrar</Button>
+        </CardFooter>
+      </Card>
     </div>
-
-  )
+  );
 }
 
-export default Login
+export default Login;
