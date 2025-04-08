60b95845ccd65549e3f86260ab8683751faaaff5
William-Cossa <devwilliamcossa@gmail.com>
2025-04-08 16:34:56 +0200

    deploy to vercel 7

--- app/(auth)/cadastro/page.tsx
+++ app/(auth)/cadastro/page.tsx
@@ -2,6 +2,7 @@ import { SignUpForm } from "@/components/Signup-form";
 import React from "react";
 
 export const dynamic = "force-dynamic";
+
 function SignupPage() {
   return (
     <main className="w-full min-h-screen flex flex-col items-center p-24">
