5e4d297993af7a294b8ded6dc2e43b5df403ec03
William-Cossa <devwilliamcossa@gmail.com>
2025-04-08 14:48:10 +0200

    fix users route

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,13 @@
-import ListUsers from '@/components/List-Users'
-import React from 'react'
+import ListUsers from "@/components/List-Users";
+import React from "react";
 
 function Dashboard() {
   return (
-    <main className='w-full min-h-screen flex flex-col items-center p-10'>
-        <h1 className='text-3xl font-bold self-start'>Dashboard</h1>
-        <ListUsers/>
+    <main className="w-full min-h-screen flex flex-col items-center p-10">
+      <h1 className="text-3xl font-bold self-start">Dashboard</h1>
+      {/* <ListUsers/> */}
     </main>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
