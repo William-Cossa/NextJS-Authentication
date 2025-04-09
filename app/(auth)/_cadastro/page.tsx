570cde3df546e549c3a1d5b2bb78a3ad6be123cf
William-Cossa <devwilliamcossa@gmail.com>
2025-04-09 10:54:38 +0200

    deploy to vercel 8

---
+++ app/(auth)/_cadastro/page.tsx
@@ -0,0 +1,14 @@
+// import { SignUpForm } from "@/components/Signup-form";
+// import React from "react";
+
+// export const dynamic = "force-dynamic";
+
+// function SignupPage() {
+//   return (
+//     <main className="w-full min-h-screen flex flex-col items-center p-24">
+//       <SignUpForm />
+//     </main>
+//   );
+// }
+
+// export default SignupPage;
---
+++ app/(auth)/_login/page.tsx
@@ -0,0 +1,13 @@
+// import { LoginForm } from "@/components/Login-form";
+// import React from "react";
+
+// export const dynamic = "force-dynamic";
+// function LoginPage() {
+//   return (
+//     <main className="w-full min-h-screen flex flex-col items-center p-24">
+//       <LoginForm />
+//     </main>
+//   );
+// }
+
+// export default LoginPage;
